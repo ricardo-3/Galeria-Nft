1128e66114985353013eabf8eb63b91b1c338f42
ricardo-3 <romero890827@gmail.com>
2022-01-16 18:51:56 -0300

    proyecto

---
+++ js.js
@@ -0,0 +1,32 @@
+//funcion ventana con querySelector //
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', ()=>{
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true')
+    } else{
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+} else{
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+}
+
+
+
+//funcion ventana con getElementById //
+function abrir(){
+    document.getElementById('popUp').style.display='block';
+}
+
+function close(){
+    document.getElementById('popUp').style.display='none';
+}
